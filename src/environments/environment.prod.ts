fcec416aaf226a746fea8704fab71a7315c5ffc0
am <am@MacBook-Pro.local>
2020-05-11 00:00:31 -0700

    these files should be created locally

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-
-  // server: "http://localhost:3001", // for testing prod in local
-  server: "http://qa.dsps.vannev.com",
-
-  // client side key for reCaptcha V3. supply your own
-  reCaptchaV3ClientKey: '6LfzjGoUAAAAAI3AQKhMdItGds9RbSKlK5gEqrex'
-};
