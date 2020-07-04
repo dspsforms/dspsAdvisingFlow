2f422b8c43ec19a3811137d2a367065e293be45b
am <am@MacBook-Pro.local>
2020-07-04 16:26:00 -0700

    deploy script not using cpan perl libs

--- dspsmisc-print/addDeploy-needsPerlLibs.pl
+++ dspsmisc-print/addDeploy-needsPerlLibs.pl
@@ -1,16 +1,14 @@
 #! /usr/bin/perl
+use 5.010;
+use open qw(:locale);
+use strict;
+use utf8;
+use warnings qw(all);
+# use Net::OpenSSH ;
+# use Net::SCP::Expect ;
+use TryCatch;
 
-# installing perl libraries on the prod server seems to be an issue
-# so using simple old perl
-
-
-# use 5.010;
-# use open qw(:locale);
-# use strict;
-# use utf8;
-# use warnings qw(all);
-# use TryCatch;
-# use File::Copy;
+use File::Copy;
 
 my $inFileName = "../dspsmisc-backend/angular-print/index.html";
 
@@ -18,7 +16,7 @@ my $outFileName = "../dspsmisc-backend/angular-print/indexModified.html";
 
 my $indexOrig = "../dspsmisc-backend/angular-print/indexOrig.html";
 
-# try {
+try {
   open(my $inHandle, '<', $inFileName);
 
   open(my $outHandle, '>', $outFileName);
@@ -39,17 +37,17 @@ my $indexOrig = "../dspsmisc-backend/angular-print/indexOrig.html";
 
   close $inHandle;
   close $outHandle;
-  print "... index.html modification done \n";
+  say "... index.html modification done";
 
-  # move ($inFileName, $indexOrig) or die "move failed: $!";
+  move ($inFileName, $indexOrig) or die "move failed: $!";
 
-  # copy($outFileName, $inFileName ) or die "Copy failed: $!";
+  copy($outFileName, $inFileName ) or die "Copy failed: $!";
 
-  # # delete outFileName
-  # unlink($outFileName)  or die "unlink failed: $!";
+  # delete outFileName
+  unlink($outFileName)  or die "unlink failed: $!";
 
-  # say "... index.html moved to $indexOrig. modifications in index.html";
+  say "... index.html moved to $indexOrig. modifications in index.html";
 
-# } catch ($err) {
-#         say "$err";
-# }
+} catch ($err) {
+        say "$err";
+}
--- dspsmisc-print/buildProd.sh
+++ dspsmisc-print/buildProd.sh
@@ -10,5 +10,19 @@ ng build --prod --deployUrl=/foo/
 
 perl addDeploy.pl
 
+# we are not able to use cpan libs in prod for some reason. so do these here
+
+inFileName=../dspsmisc-backend/angular-print/index.html
+
+outFileName=../dspsmisc-backend/angular-print/indexModified.html
+
+indexOrig=../dspsmisc-backend/angular-print/indexOrig.html
+
+mv $inFileName $indexOrig
+cp $outFileName $inFileName
+rm $outFileName
+
+
+
 
 
