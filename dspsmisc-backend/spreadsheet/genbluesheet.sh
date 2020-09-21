4d112901511fafd04ff415fec1664dcef10e388c
am <am@MacBook-Pro.local>
2020-09-21 14:30:45 -0700

    stuff

--- dspsmisc-backend/spreadsheet/genbluesheet.sh
+++ dspsmisc-backend/spreadsheet/genbluesheet.sh
@@ -1,1 +1,10 @@
-mongoexport --host="10.3.105.175:27017" --db="dspsmisc" --collection="bluesheets" --type=csv  --fieldFile="bluesheet_fieldNames.txt"  --out="bluesheet_spreadsheet.csv"
+
+HOST=10.3.105.175:27017
+DB=dspsmisc
+COLLECTION=bluesheets
+FIELD_FILE=bluesheet_fieldNames.txt
+
+# TODO: generate year, mon, day 
+OUT_FILE=bluesheet-2010-09-21.csv
+
+mongoexport --host="${HOST}" --db="${DB}" --collection="${COLLECTION}" --type=csv  --fieldFile="${FIELD_FILE}"  --out="${OUT_FILE}"
