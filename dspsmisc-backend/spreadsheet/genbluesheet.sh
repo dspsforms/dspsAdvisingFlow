d518f9553bd4c6faffe70c0d74fd9995a0d3c384
am <am@MacBook-Pro.local>
2020-09-21 14:36:48 -0700

    checkpoint

--- dspsmisc-backend/spreadsheet/genbluesheet.sh
+++ dspsmisc-backend/spreadsheet/genbluesheet.sh
@@ -5,6 +5,11 @@ COLLECTION=bluesheets
 FIELD_FILE=bluesheet_fieldNames.txt
 
 # TODO: generate year, mon, day 
+OUT_FILE_STEP1=bluesheet-2010-09-21.step1.csv
+
 OUT_FILE=bluesheet-2010-09-21.csv
 
-mongoexport --host="${HOST}" --db="${DB}" --collection="${COLLECTION}" --type=csv  --fieldFile="${FIELD_FILE}"  --out="${OUT_FILE}"
+mongoexport --host="${HOST}" --db="${DB}" --collection="${COLLECTION}" --type=csv  --fieldFile="${FIELD_FILE}"  --out="${OUT_FILE_STEP1}"
+
+
+sed 's/formWithLatestHistory.//g' < ${OUT_FILE_STEP1} > ${OUT_FILE}
